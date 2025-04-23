aba06a402b428a826c554f77174daaa94a11bf8f
Carlitos Dutan <carlitosdutan@pursuit.org>
2025-04-23 16:25:34 -0400

    completed exam

--- src/main/java/org/example/CarInventory.java
+++ src/main/java/org/example/CarInventory.java
@@ -5,34 +5,62 @@ import java.util.List;
 import java.util.Map;
 
 public class CarInventory {
-   
+
+    Map<String, Car> carMap = new HashMap<>();
 
     public CarInventory() {
       
     }
 
     public void addCar(Car car) {
-     
+        if (car == null || carMap.containsKey(car.getModel())) {
+            throw new IllegalArgumentException();
+        }
+        carMap.put(car.getModel(), car);
     }
 
     public Car getCar(String model) {
-      
+        if (carMap.containsKey(model)) {
+            return carMap.get(model);
+        }
+        else {
+            return null;
+        }
     }
 
     public Car sellCar(String model) {
-      
+      Car foundCar = carMap.get(model);
+
+      if (foundCar != null) {
+          carMap.remove(model);
+          return foundCar;
+      }
+      else {
+          return null;
+      }
     }
 
     public void updateCarPrice(String model, double newPrice) {
-      
+      Car foundCar = carMap.get(model);
+
+      if (foundCar != null) {
+          foundCar.setPrice(newPrice);
+      }
     }
 
     public List<Car> findCarsByMake(String make) {
-    
+        List<Car> filteredCars = new ArrayList<>();
+
+        carMap.forEach((key, value) -> {
+            if (value.getMake().equals(make)) {
+                filteredCars.add(carMap.get(key));
+            }
+        });
+        return filteredCars;
     }
 
     public int getCarCount() {
-        
+        return carMap.size();
     }
 
     public static void main( String[] args )
